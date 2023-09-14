8ca1f0e7ed5f51f31fe8da92b945bfc3e0b46272
Gabriel Manhães <gabriel@kali.com>
2023-09-14 03:51:03 -0300

    almost there

---
+++ purge.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+rm -rf nodes/fullnode-jd-1/data/caches
+rm -rf nodes/fullnode-jd-1/data/database
+rm -rf nodes/fullnode-jd-1/data/fastsync
+rm -rf nodes/fullnode-jd-1/data/besu.networks
+rm -rf nodes/fullnode-jd-1/data/besu.ports
+rm -rf nodes/fullnode-jd-1/data/DATABASE_METADATA.json
+rm -rf nodes/fullnode-jd-1/data/jwt.hex
+
+rm -rf nodes/fullnode-jd-2/data/caches
+rm -rf nodes/fullnode-jd-2/data/database
+rm -rf nodes/fullnode-jd-2/data/fastsync
+rm -rf nodes/fullnode-jd-2/data/besu.networks
+rm -rf nodes/fullnode-jd-2/data/besu.ports
+rm -rf nodes/fullnode-jd-2/data/DATABASE_METADATA.json
+rm -rf nodes/fullnode-jd-2/data/jwt.hex
+
+rm -rf nodes/validator-bcb-1/data/caches
+rm -rf nodes/validator-bcb-1/data/database
+rm -rf nodes/validator-bcb-1/data/fastsync
+rm -rf nodes/validator-bcb-1/data/besu.networks
+rm -rf nodes/validator-bcb-1/data/besu.ports
+rm -rf nodes/validator-bcb-1/data/DATABASE_METADATA.json
+rm -rf nodes/validator-bcb-1/data/jwt.hex
+
+rm -rf nodes/validator-bcb-2/data/caches
+rm -rf nodes/validator-bcb-2/data/database
+rm -rf nodes/validator-bcb-2/data/fastsync
+rm -rf nodes/validator-bcb-2/data/besu.networks
+rm -rf nodes/validator-bcb-2/data/besu.ports
+rm -rf nodes/validator-bcb-2/data/DATABASE_METADATA.json
+rm -rf nodes/validator-bcb-2/data/jwt.hex
+
+rm -rf nodes/validator-selic-1/data/caches
+rm -rf nodes/validator-selic-1/data/database
+rm -rf nodes/validator-selic-1/data/fastsync
+rm -rf nodes/validator-selic-1/data/besu.networks
+rm -rf nodes/validator-selic-1/data/besu.ports
+rm -rf nodes/validator-selic-1/data/DATABASE_METADATA.json
+rm -rf nodes/validator-selic-1/data/jwt.hex
+
+rm -rf nodes/validator-selic-2/data/caches
+rm -rf nodes/validator-selic-2/data/database
+rm -rf nodes/validator-selic-2/data/fastsync
+rm -rf nodes/validator-selic-2/data/besu.networks
+rm -rf nodes/validator-selic-2/data/besu.ports
+rm -rf nodes/validator-selic-2/data/DATABASE_METADATA.json
+rm -rf nodes/validator-selic-2/data/jwt.hex
