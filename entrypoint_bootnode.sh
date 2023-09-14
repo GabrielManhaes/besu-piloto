eec6c7b46846a5ce363643cd4344f3ac9758bc08
Gabriel Manhães <gabriel@kali.com>
2023-09-14 03:52:07 -0300

    deleting scripts and adding .gitignore

--- entrypoint_bootnode.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-besu --data-path=$BESU_IDENTITY/data --genesis-file=$BESU_IDENTITY/genesis.json --p2p-port=$P2P_PORT --rpc-http-enabled --rpc-http-api=ETH,NET,QBFT --host-allowlist="*" --rpc-http-cors-origins="all"
